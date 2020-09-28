286f6c191f8e50c4d31d5721872d469afd23ad80
KevinHooij <kevin.hooijschuur@hva.nl>
2020-09-28 22:46:40 +0200

    Completes Level 1 Task 1

--- app/src/main/java/com/example/madlevel1task1final/HigherLowerActivity.kt
+++ app/src/main/java/com/example/madlevel1task1final/HigherLowerActivity.kt
@@ -2,10 +2,83 @@ package com.example.madlevel1task1final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1task1final.databinding.ActivityHigherLowerBinding
 
 class HigherLowerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_higher_lower)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
     }
+
+    private fun updateUI(){
+        binding.tvLastThrow.text = lastThrow.toString()
+
+        when (currentThrow) {
+            1 -> binding.ivDice.setImageResource(R.drawable.dice1)
+            2 -> binding.ivDice.setImageResource(R.drawable.dice2)
+            3 -> binding.ivDice.setImageResource(R.drawable.dice3)
+            4 -> binding.ivDice.setImageResource(R.drawable.dice4)
+            5 -> binding.ivDice.setImageResource(R.drawable.dice5)
+            6 -> binding.ivDice.setImageResource(R.drawable.dice6)
+        }
+    }
+
+    private fun initViews(){
+        binding.btnHigher.setOnClickListener { onHigherClick() }
+        binding.btnLower.setOnClickListener { onLowerClick() }
+        binding.btnEquals.setOnClickListener { onEqualsClick() }
+        updateUI()
+    }
+    
+    private fun rollDice(){
+        lastThrow = currentThrow
+        currentThrow = (1..6).random()
+        updateUI()
+    }
+
+    private fun onHigherClick(){
+        rollDice()
+        if (lastThrow < currentThrow){
+            onAnswerCorrect()
+        } else{
+            onAnswerIncorrect()
+        }
+    }
+
+    private fun onLowerClick() {
+        rollDice()
+        if (lastThrow > currentThrow) {
+            onAnswerCorrect()
+        } else {
+            onAnswerIncorrect()
+        }
+    }
+
+    private fun onEqualsClick() {
+        rollDice()
+        if (lastThrow == currentThrow) {
+           onAnswerCorrect()
+        } else {
+           onAnswerIncorrect()
+        }
+    }
+
+    private fun onAnswerCorrect(){
+        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+    }
+
+    private fun onAnswerIncorrect(){
+        Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+    }
+
+
 }
